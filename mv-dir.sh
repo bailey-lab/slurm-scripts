96a2299aa1d42ae48389880e3f6dd7c41cb51862
Aris Paschalidis <aris.paschalidis@gmail.com>
2022-02-16 16:18:50 -0500

    Add missing pound to trigger sbatch commands

--- mv-dir.sh
+++ mv-dir.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Request an hour of runtime:
-SBATCH --time=1:00:00
+#SBATCH --time=1:00:00
 
 # Default resources are 1 core with 2.8GB of memory.
 # Use more memory (4GB):
 #SBATCH --mem=4G
 
 # Specify a job name:
-SBATCH -J move-dir
+#SBATCH --job-name=move-dir
 
 # Notify details about job
-SBATCH --mail-type=ALL
+#SBATCH --mail-type=ALL
 
 # Run a command
 mv <source/dir/path> <destination/dir/path>
