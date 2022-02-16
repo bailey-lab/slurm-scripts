3d4c42fb0c2a85df232bce7f9e426e11e1bc15ad
Aris Paschalidis <aris.paschalidis@gmail.com>
2022-02-16 17:35:56 -0500

    Improve comments

--- download.sh
+++ download.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Request four hours of runtime:
+# Set runtime:
 #SBATCH --time=4:00:00
 
-# Default resources are 1 core with 2.8GB of memory.
-# Use more memory (4GB):
+# Set memory allocation:
+# Default on Oscar is 2.8GB of memory
 #SBATCH --mem=4G
 
 # Specify a job name:
@@ -18,4 +18,5 @@
 dir="download-vcfs"
 url="ftp://ngs.sanger.ac.uk/production/malaria/pfcommunityproject/Pf6/Pf_6_vcf"
 
+# Download data
 wget --recursive --no-host-directories --cut-dirs=5 --directory-prefix=${dir} --accept "*vcf.gz" ${url}
