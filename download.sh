7768db7f49fda55791740d37f650d8252c22f4c4
Aris Paschalidis <aris.paschalidis@gmail.com>
2022-02-16 16:25:47 -0500

    Add wget download script

---
+++ download.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Request four hours of runtime:
+#SBATCH --time=4:00:00
+
+# Default resources are 1 core with 2.8GB of memory.
+# Use more memory (4GB):
+#SBATCH --mem=4G
+
+# Specify a job name:
+#SBATCH --job-name=download-data
+
+# Notify details about job
+#SBATCH --mail-type=ALL
+
+# Define variables
+dir="download-vcfs"
+url="ftp://ngs.sanger.ac.uk/production/malaria/pfcommunityproject/Pf6/Pf_6_vcf"
+
+wget --recursive --no-host-directories --cut-dirs=5 --directory-prefix=${dir} --accept "*vcf.gz" ${url}
